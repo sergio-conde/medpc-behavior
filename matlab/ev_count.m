cada82652e3588b175c50e472f362f854d2c6fb2
Sergio Conde <conde@herseninstituut.knaw.nl>
2026-02-19 16:13:52 +0100

    started error handling

--- matlab/ev_count.m
+++ matlab/ev_count.m
@@ -1,5 +1,17 @@
 function ev = ev_count(cfg)
 
+% check whether med_data is a path, a struct (i.e, after using read_medpc
+% independently) or a vector (the event and time blocks)
+
+%%%% Check input struct %%%%%%
+if isfield(cfg,'med_data')
+    
+else
+    error_handle(0)
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 ev.cfg = cfg;
 
 med_data = read_medpc(cfg.med_file);
@@ -13,3 +25,11 @@ for iev = 1:length(ev_labels)
     ev.(ev_labels{iev}).timestamps  = med_data.D(ev_flags) * 10e-3;
 end
 
+function error_handle(error_id)
+
+switch error_id
+    case 0
+        fprintf('\n')
+        error('Input structure must contain a med_data field')
+    otherwise
+end
