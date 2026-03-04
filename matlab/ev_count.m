ba3f4d6879544e71005bb7fa93e66da52dbb54bc
Sergio Conde <124182273+sergio-conde@users.noreply.github.com>
2026-03-04 17:07:50 +0100

    add input check + error handling

--- matlab/ev_count.m
+++ matlab/ev_count.m
@@ -3,19 +3,15 @@ function ev = ev_count(cfg)
 % check whether med_data is a path, a struct (i.e, after using read_medpc
 % independently) or a vector (the event and time blocks)
 
-%%%% Check input struct %%%%%%
+%%%% Check input struct %%%%%%%%%%%%%%%%%%
 if isfield(cfg,'med_data')
-    
+    med_data = get_med_data(cfg.med_data);
 else
     error_handle(0)
 end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 ev.cfg = cfg;
-
-med_data = read_medpc(cfg.med_file);
-
 ev_labels = fieldnames(cfg.events);
 for iev = 1:length(ev_labels)
     ev_id     = cfg.events.(ev_labels{iev});
@@ -25,11 +21,29 @@ for iev = 1:length(ev_labels)
     ev.(ev_labels{iev}).timestamps  = med_data.D(ev_flags) * 10e-3;
 end
 
-function error_handle(error_id)
+function med_data = get_med_data(med_data_in)
+    if ischar(med_data_in)
+        med_data = read_medpc(med_data_in);
+    elseif isstruct(med_data_in) % not any struct! include some output from read_medpc so I can identify that is the correct struct
+        % if med_str_chk(med_data_in)
+        med_data = med_data_in;
+        % else
+        % error_handle(2)
+        % end
+    else
+        error_handle(1)
+    end
 
+
+function error_handle(error_id)
 switch error_id
     case 0
         fprintf('\n')
         error('Input structure must contain a med_data field')
-    otherwise
+    case 1
+        fprintf('\n')
+        error('med_data field must contain either a path or a struct')
+    case 2
+        fprintf('\n')
+        error('med_data struct is uncompatible')
 end
