76f7974a1ca543c5f4bd69e4c9d8647bfcc3f05e
sergio conde-ocazionez <s.conde@nin.knaw.nl>
2026-08-13 17:10:39 +0200

    Update multipellet_example.m

--- example_use/multipellet_example.m
+++ example_use/multipellet_example.m
@@ -5,18 +5,19 @@
 % analysis from the MedPC output file. 
 
 clear; clc
-ref_file = 'M:\GitHub\medpc-behavior\example_data\example_rat_multipellet';
+% ref_file = 'M:\GitHub\medpc-behavior\example_data\example_rat_multipellet';
+ref_file = 'C:\Work\GitHub\medpc-behavior\example_data\example_rat_multipellet';
 % med_data = read_medpc(ref_file);
 
 % Configuration
 % We start by defining a configuration struct. So far, this struct must have 
 % at least the following fields:
-%% 
+% % 
 % * _*med_file*_: full path of the file to be analyzed [char]
 
 cfg           = [];
 cfg.med_file  = ref_file;
-%% 
+% % 
 % * _*events*_: this field contains as many fields as events you want to include 
 % in the analysis. It can iclude, for example, cues, outcomes, levers, trial start, 
 % etc. You can define the name os thse fields on your convenience. 
@@ -38,13 +39,13 @@ cfg.events.subs_4p    = 12;   % \ 12 - 4p subsequent pellets
 cfg.events.mag        = 16;   % \ 16 - Mag entry any time
 cfg.events.sess_end   = 100;  % \ 100 - End of session
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% 
+% % 
 % * _*trial*_: which has felds defining the key events. These include events 
 % that mark the start and the end of the trial
 
-cfg.trial.start_label = {'1p','4p'};
-cfg.trial.start       = {'cue1_on','cue4_on'};
-cfg.trial.end         = {'cue1_off','cue4_off'};
+cfg.trial.trialLabel = {'1p','4p'};
+cfg.trial.start      = {'cue1_on','cue4_on'};
+cfg.trial.end        = {'cue1_off','cue4_off'};
 
 % Create main trial structure
 % We can use that configuration to create the main trial structure. This struct 
@@ -58,31 +59,34 @@ cfg.trial.end         = {'cue1_off','cue4_off'};
 % will be the reference to the trial based analysis: counting behavioral events, 
 % latencies, etc.
 
-trial_struct = get_trials(cfg);
+trial_struct = getTrials(cfg);
 % Add selected variables (from the cfg.events)
 % After having the main trial struct, you can add behavioral variables (events) 
 % to that structure by listing the field names defined in the event configuration 
 % and corresponding to the variables of your interest. In this example, we are 
 % adding the 'mag' variable, which indicates a maganize entry. 
 
-sel_events  = {'mag_cue1','mag_cue4','mag'};
+evConfig.events  = {'mag_cue1','mag_cue4','mag'};
 % sel_events  = {'mag'};
-ev_struct   = add_var(trial_struct,sel_events);
+ev_struct   = addEvent(trial_struct,evConfig);
 % Extract data of interest
 
 clear out_files
 entry                 = [];
-entry.num             = {7};
-entry.ref.num         = {'higher'};
-entry.int_label       = {'trial'};
-[out_files,file_line] = get_entry(ev_struct.trials,entry);
+entry.count           = [7 12];
+entry.contrast.count  = 'range';
+entry.interval        = 'trial';
+[out_files,requestConfig] = getEntry(ev_struct.trials,entry);
+
+%%
 % Extract data of interest
 
-data_1p_tr  = pick_files(ev_struct.trials,'type','1p','int_label','trial');
-data_1p_iti = pick_files(ev_struct.trials,'type','1p','int_label','iti');
+data_1p_tr  = getEntry(ev_struct.trials,'trialLabel','1p','interval','trial');
+data_1p_iti = getEntry(ev_struct.trials,'trialLabel','1p','interval','iti');
 
-data_4p_tr  = pick_files(ev_struct.trials,'type','4p','int_label','trial');
-data_4p_iti = pick_files(ev_struct.trials,'type','4p','int_label','iti');
+data_4p_tr  = getEntry(ev_struct.trials,'trialLabel','4p','interval','trial');
+data_4p_iti = getEntry(ev_struct.trials,'trialLabel','4p','interval','iti');
+
 % Plot some results
 
 box_data  = [[data_1p_tr.mag_cue1_num] [data_1p_iti.mag_cue1_num] ...
@@ -103,18 +107,3 @@ boxplot(box_data,gr_id)
 box off; ylabel '# mag (any) entries'
 xticklabels({'cue1p','iti1p','cue4p','iti4p'})
 
-wfig(2)
-
-box_data  = [[data_1p_tr.mag_cue1_lat1] [data_4p_tr.mag_cue4_lat1]];
-gr_id     = [ones(1,length(data_1p_tr)) 2*ones(1,length(data_4p_tr))];
-subplot 121
-boxplot(box_data,gr_id)
-box off; ylabel '# mag (1p / 4p) entry latency'
-xticklabels({'cue1p','cue4p'}); 
-
-box_data  = [[data_1p_iti.mag_lat1] [data_4p_iti.mag_lat1]];
-gr_id     = [ones(1,length(data_1p_iti)) 2*ones(1,length(data_4p_iti))];
-subplot 122
-boxplot(box_data,gr_id)
-box off; ylabel '# mag (any) entry latency'
-xticklabels({'iti1p','iti4p'});
