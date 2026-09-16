5f6c1de8738806294c53fb2b6d709ec75d6e72f9
Sergio Conde <124182273+sergio-conde@users.noreply.github.com>
2026-09-16 17:05:06 +0200

    Update addEvent.m

--- matlab/addEvent.m
+++ matlab/addEvent.m
@@ -1,17 +1,23 @@
 function eventList = addEvent(trialEntry,cfg)
 
+MEDSAMPLERATE = 10e-3;
+
 cfg = checkCfg(cfg); % check input data and requestes fields
 
 eventList.cfg = rmfield(cfg,'inputFields');
 eventList.dataCfg = trialEntry.cfg;
 eventList.medData = trialEntry.medData;
 
+medTime = trialEntry.medData.(trialEntry.cfg.medTime);
+medEvents = trialEntry.medData.(trialEntry.cfg.medEvents);
+
 trials = trialEntry.trials;
 eventRequest = cfg.events;
 
 for ievent = 1:length(eventRequest)
-    eventFlags = eventList.medData.E == trialEntry.cfg.events.(eventRequest{ievent});
-    eventTimes = eventList.medData.D(eventFlags) * 10e-3;
+    eventLabel = eventRequest{ievent};
+    eventFlags = medEvents == trialEntry.cfg.events.(eventLabel);
+    eventTimes = medTime(eventFlags) * MEDSAMPLERATE;
     for ientry = 1:size(trials,1)
         
         entryFlags = eventTimes >= trials(ientry).startTime & ...
@@ -19,20 +25,32 @@ for ievent = 1:length(eventRequest)
         entryTimes = eventTimes(entryFlags) - trials(ientry).startTime;
         
         % count events. 
-        trials(ientry).([eventRequest{ievent} 'Count']) = numel(entryTimes);
+        trials(ientry).([eventLabel 'Count']) = numel(entryTimes);
 
         % compute event latency if requested
         if cfg.latency
-            trials(ientry).([eventRequest{ievent} 'Times']) = entryTimes;
+            trials(ientry).([eventLabel 'Times']) = entryTimes;
         end
 
         % extract first event if requested
         if cfg.firstEvent
             firstEvent  = min(entryTimes);
             if isempty(firstEvent); firstEvent = NaN; end
-            trials(ientry).([eventRequest{ievent} 'First']) = firstEvent;
+            trials(ientry).([eventLabel 'First']) = firstEvent;
         end
-        
+
+        % extract event bouts
+        if isfield(cfg,'bouts') && ~isempty(entryTimes)
+            boutList = extractBouts(entryTimes, ...
+                cfg.bouts.minInterval(ievent), ...
+                cfg.bouts.minDuration(ievent));
+            nBouts = height(boutList);
+            
+            trials(ientry).([eventLabel 'Bouts']) = nBouts;
+            trials(ientry).([eventLabel 'BoutStart']) = boutList.boutStart;
+            trials(ientry).([eventLabel 'BoutEnd']) = boutList.boutEnd;
+            trials(ientry).([eventLabel 'BoutDuration']) = boutList.duration;
+        end
     end
 end
 
